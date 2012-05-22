992ce73a915572221f3db7e30ba20072eaada32d
Israel Shirk <israel@wearemotive.com>
2012-05-22 16:18:04 -0600

    Alter-database now works with Sequel Pro quotes

--- alter-database.py
+++ alter-database.py
@@ -9,7 +9,7 @@ sites = {
 }
 
 
-
+DEBUG = 0
 
 
 
@@ -30,8 +30,20 @@ for site in sites:
         db_content = fd.read()
         fd.close()
 
-        quote_matcher = re.compile(r'(?<![\\])"', re.MULTILINE)
-        old_site_matcher = re.compile("(www\.)?" + site['old_domain'], re.MULTILINE)
+        quote_type_matcher = re.compile(r'(;s:[0-9]+:\\")', re.MULTILINE)
+
+        match = quote_type_matcher.search(db_content)
+
+        if (match):
+            quote_matcher = re.compile(r'(?<=\\)"', re.MULTILINE)
+            print 'Matching with (?<=\\)"'
+        else:
+            quote_matcher = re.compile(r'(?<![\\])"', re.MULTILINE)
+            print 'Matching with (?<![\\])"'
+
+        old_site_matcher = re.compile("(www\.)?" + site['old_domain'].replace('.', '\.'), re.MULTILINE)
+
+        serialized_matcher = re.compile(r'([;{][sS]:[0-9]+:)', re.MULTILINE)
 
         the_start = 0
         match = 1
@@ -59,32 +71,49 @@ for site in sites:
                 db_content = db_content[:the_start] + site['new_domain'] + db_content[the_end:]
                 continue
 
-            section_start_pos = section_start_match.start() + 1
+            section_start_pos = section_start_match.start() - 15
             section_end_pos = section_end_match.start()
 
             section = db_content[section_start_pos:section_end_pos]
 
-            pre = db_content[:section_start_pos]
-            post = db_content[section_end_pos:]
-
-            serialized_match = re.search(r'[a-zA-Z]:[0-9]+:{', section)
+            serialized_match = serialized_matcher.search(section)
 
             if not serialized_match:
+                if DEBUG: print "Unserialized: " + db_content[the_start - 50:the_end] + "\nsection: " + section
                 db_content = db_content[:the_start] + site['new_domain'] + db_content[the_end:]
             else:
+                if DEBUG: print "Serialized: " + db_content[the_start - 50:the_end] + "\nsection: " + section
                 group = match.group()
 
                 len_start = db_content.rfind("s:", 0, the_start) + 2
                 len_end = db_content.find(":", len_start)
 
-                try:
-                    old_len = int(db_content[len_start:len_end])
-                except:
+                if (len_end - len_start > 10):
                     db_content = db_content[:the_start] + site['new_domain'] + db_content[the_end:]
                     continue
 
+                if DEBUG: print "Looking for length in: %s" % db_content[len_start:len_end]
+
+                try:
+                    old_len = int(db_content[len_start:len_end])
+                except:
+                    print "Unexpected error, trying to keep going:", sys.exc_info(), serialized_match.groups()
+                    print "Context: ", db_content[the_start-100:the_end+100]
+                    db_content = db_content[:the_start] + site['new_domain'] + db_content[the_end:]
+                    continue
+
+                if DEBUG: print "old len: ", old_len
+
+                if DEBUG: print "group: " + group
+
                 len_diff = len(site['new_domain']) - len(group)
                 new_len = old_len + len_diff
+
+                if DEBUG: print "len diff: %s " % len_diff
+
+                if DEBUG: print "new len: ", new_len
+
+                print "Replaced serialized: " + db_content[len_start - 2: the_end]
 
                 db_content = db_content[:len_start] + str(new_len) + db_content[len_end:]
 
@@ -95,12 +124,11 @@ for site in sites:
                 the_end = match.end()
 
                 db_content = db_content[:the_start] + site['new_domain'] + db_content[the_end:]
+                print "With: " + db_content[len_start - 2: the_end + len_diff]
 
-        fd = open(site['database_file'] + '_modified.sql', "w")
-        fd.write(db_content)
-        fd.close()
-
-        del(db_content)
+        out = open(site['database_file'] + '_modified.sql', "w")
+        out.write(db_content)
+        out.close()
 
         print "New database is in", site['database_file'] + '_modified.sql'
 
